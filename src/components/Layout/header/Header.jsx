69aab6d7de68fe71364fff59a8df2c32fcd6c7e6
Atai Abdiev <atai.abdiev.dev@gmail.com>
2024-02-29 21:35:37 +0600

    fix

--- src/components/Layout/header/Header.jsx
+++ src/components/Layout/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <img
               className={styles.img1}
               alt="logo"
-              src="https://s3-alpha-sig.figma.com/img/142b/6dac/c0ebc4d537c0c9ae10fdae59223c67fc?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Sos13AWarvbKtat14fvcbzyz8hSKVIgeU4xkseQY9BcQW1pBVO8iqCmIzbrphKZ~liq0CHBRJT2hpyuQ1-ERUKcTMu5voqGD~kfEz9cztQaQdB2THuGDof1hmCTEzlUgUdoxkAJijY5ej-VWuibIvFbCy-uOwLl5327bhym6QgvrZDmh84l6J-Q0HNYDodJSye~Pee0BCzU~81WMkNGTtkBoPmyXETyeSAnWsBWdou7fXJjBCbG7KVCl5hu839ImY6dVqwERevPZbd8bcObq-vtTMQ9yyNi6zcAHo-GoF~wLhdjE5DqxvUP77tQbQhTiqR1kDaEF8me-kqNYGGmbzA__"
+              src="https://my.alatoo.edu.kg/images/logo_aiu.png"
             />
             <div className={styles.header_wrap__logo__spans}>
               <span className={styles.span1}>Connect</span>
