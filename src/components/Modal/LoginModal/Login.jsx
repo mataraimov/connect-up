1d09ab05c2bd4a983ddd9bc273626a2e3c194757
Nuradil Mataraimov <nuradil.mataraimov@alatoo.edu.kg>
2024-05-31 01:40:32 +0600

    login

--- src/components/Modal/LoginModal/Login.jsx
+++ src/components/Modal/LoginModal/Login.jsx
@@ -4,9 +4,13 @@ import { Form, Input, Button, message } from 'antd';
 import { useAuth } from '../../utils/context';
 import { API_URL } from '../../utils/config';
 import { useNavigate } from 'react-router-dom';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './Login.css';
+
 const Login = () => {
   const { setAuthData } = useAuth();
   const navigate = useNavigate();
+
   const handleLogin = async (values) => {
     try {
       const response = await axios.post(`${API_URL}/users/login/`, values);
@@ -16,28 +20,39 @@ const Login = () => {
       message.success('Login successful');
       navigate('/admin-events');
     } catch (error) {
-      console.error('Error:', error);
       message.error('Invalid username or password');
     }
   };
 
   return (
-    <Form onFinish={handleLogin}>
-      <Form.Item name="email" rules={[{ required: true, message: 'Please input your username!' }]}>
-        <Input placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password placeholder="Password" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
+    <div className="login-container">
+      <div className="login-form">
+        <h2 className="login-title">Welcome Back</h2>
+        <Form onFinish={handleLogin} className="form">
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Email" className="input-field" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              className="input-field"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-button">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
